1a2aa67d84f1130498bb33e341d33b66faca04da
nipunnishamaheeka <nishamaheeka123@gmail.com>
2024-08-04 09:59:43 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse68/beans/Item.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse68.beans;
+
+public class Item {
+}
---
+++ src/main/java/lk/ijse/gdse68/config/Config.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse68.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("lk.ijse.gdse68")
+public class Config {
+}
